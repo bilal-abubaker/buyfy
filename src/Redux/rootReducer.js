3c02f5483fce2647d0b59dc14e2a87217547a4f6
Bilal abubaker <bilal_abubaker@hotmail.com>
2020-07-27 00:20:01 +0500

    setup redux store

---
+++ src/Redux/rootReducer.js
@@ -0,0 +1,10 @@
+import {combineReducers} from "redux"
+import userReducer from "./user/userReducer";
+
+var rootReducer =  combineReducers({
+
+    currentUser: userReducer
+})
+
+
+export default rootReducer;
---
+++ src/Redux/user/userReducer.js
@@ -0,0 +1,13 @@
+var initialState = null
+
+var userReducer  = (state = initialState, action) => {
+
+    var {type, payload} = action;
+    
+    switch (type) {
+       
+        default:
+            return state;
+    }
+}
+export default userReducer;
